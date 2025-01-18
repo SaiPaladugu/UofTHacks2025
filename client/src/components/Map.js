13c4305c8521a2edceef35d31fa1d39bb8ac9464
KlausMikhaelson <satyamsingh5076@gmail.com>
2025-01-18 15:32:33 -0500

    added hotspot,and tailwind

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -12,7 +12,29 @@ const MyMap = () => {
         mapboxgl.accessToken = 'pk.eyJ1Ijoic2FpcGFsYWR1Z3UiLCJhIjoiY202MmxmMTA4MTRnYTJqb3A3dGh1ajc0ayJ9.yct_mqWxLmGeEmr86O7ezA'
         mapRef.current = new mapboxgl.Map({
             container: mapContainerRef.current,
+            center: [-74.0060152, 40.7127281],
         });
+
+
+        mapRef.current.on('style.load', () => {
+            mapRef.current.addSource('urban-areas', {
+                type: 'geojson',
+                data: 'https://docs.mapbox.com/mapbox-gl-js/assets/ne_50m_urban_areas.geojson'
+            });
+
+            mapRef.current.addLayer({
+                id: 'urban-areas-fill',
+                type: 'fill',
+                slot: 'middle',
+                source: 'urban-areas',
+                layout: {},
+                paint: {
+                    'fill-color': '#f08',
+                    'fill-opacity': 0.4
+                }
+            });
+        });
+
 
         return () => {
             mapRef.current.remove()
